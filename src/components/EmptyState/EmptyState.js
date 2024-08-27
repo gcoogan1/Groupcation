24ab33fb103b970b56b8bb87eb80b35784d8bd10
Genevieve Coogan <gcoogan1@gmail.com>
2024-08-27 14:54:19 -0400

    Adjust styling to empty state

--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -6,8 +6,8 @@ import { emptyStateStyles } from "./styles/EmptyState.styles";
 /**
  * This component renders a container with text above a button.
  * @prop {string} text required -> text to be displayed above the button
- * @prop {function} onPress required -> onPress event for button
- * @prop {string} label required -> label of button
+ * @prop {function} onPress optional -> onPress event for button
+ * @prop {string} label optional -> label of button
  * @prop {svg} iconLeft optional -> displays icon (set color in icon element) to the right of text
  * @returns {ReactElement} Renders a container with text and a button.
  *
@@ -15,11 +15,15 @@ import { emptyStateStyles } from "./styles/EmptyState.styles";
  */
 const EmptyState = ({ text, onPress, label, iconLeft }) => {
   return (
-    <View style={emptyStateStyles.container}>
+    <View
+      style={[emptyStateStyles.container, !label && { borderStyle: "dashed" }]}
+    >
       <Text style={emptyStateStyles.text}>{text}</Text>
-      <Button buttonType={"secondary"} onPress={onPress} iconLeft={iconLeft}>
-        {label}
-      </Button>
+      {label && (
+        <Button buttonType={"secondary"} onPress={onPress} iconLeft={iconLeft}>
+          {label}
+        </Button>
+      )}
     </View>
   );
 };
